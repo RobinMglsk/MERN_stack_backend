5208c6d2609e9f60240d5f516503ce65c5ffaa71
Robinmglsk <robin@migalski.be>
2018-05-17 22:43:21 +0100

    Removed keys from git

---
+++ config/keys.default.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://<user>:<pass>@<server>:<port>/<collection>',
+  secretKey: 'hv1SljXqCxSL8uMcTh#!5Ch^6Kxv1D'
+};
--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI:'mongodb://admin@192.168.0.40:27017/devconnector',
-    secretKey: 'hv1SljXqCxSL8uMcTh#!5Ch^6Kxv1D',
-};
