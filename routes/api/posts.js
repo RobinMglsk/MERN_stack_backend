c42e919c6121c4f2dc1c0ba38cd0cb9af23fe4f5
Robinmglsk <robin@migalski.be>
2018-05-21 14:33:27 +0100

    Added posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -185,4 +185,85 @@ router.post(
   }
 );
 
+/**
+ * @route   POST api/post/comment/:id
+ * @desc    Add comment to post
+ * @access  Private
+ */
+
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        // Add to comments array
+        post.comments.unshift(newComment);
+
+        // Save
+        post
+          .save()
+          .then(post => res.json(post))
+          .catch(err =>
+            res.status(404).json({ postNotFound: `No post found` })
+          );
+      })
+      .catch(err => res.status(500).json(err));
+  }
+);
+
+/**
+ * @route   DELETE  api/post/comment/:id/:comment_id
+ * @desc    Delete comment to post
+ * @access  Private
+ */
+
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        // Check if comment exists
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return Response.satatus(404).json({
+            commentNotExists: `Comment does not exists`
+          });
+        }
+
+        // Get remove index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        // Splice comment out of array
+        post.comments.splice(removeIndex, 1);
+
+        post
+          .save()
+          .then(post => res.json(post))
+          .catch(err => res.status(500).json(err));
+      })
+      .catch(err => res.status(500).json(err));
+  }
+);
+
 module.exports = router;
